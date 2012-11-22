11ddb6b00016058aa391ab6a37b128b8cafc4ed4
Aaron Randall <aaron@songkick.com>
2012-11-22 11:03:48 +0000

    Added local definitions file so we can add/test definitions quicker.

--- scripts/songkick/globals.js
+++ scripts/songkick/globals.js
@@ -11,18 +11,27 @@ var storageKey = 'artistLookupDefinitions';
 var storage = chrome.storage.local;
 
 // Uncomment to clear localStorage key
-//storage.remove(storageKey);
+console.log("localStorage for Chromekick is being cleared")
+storage.remove(storageKey);
 
-storage.get(storageKey,function(response){
-  if (!response[storageKey]) {
-    console.log(storageKey + " doesn't exist in localStorage, attempting to populate now");
-    Service.get(Constants.definitionsUri, null, definitionsCallback);
-  } else {
-    console.log(storageKey + " value is stored already, as:", response);
-    Globals.artistLookupDefinitions = response["artistLookupDefinitions"];
-    console.log("set artistlookupdefs to :" + Globals.artistLookupDefinitions);
-  }
-});
+console.log('loading definitions file from local');
+var loadDefinitionsFromFile = true;
+
+if (loadDefinitionsFromFile) {
+  Globals.artistLookupDefinitions = localArtistLookupDefinitions;
+} else {
+
+  storage.get(storageKey,function(response){
+    if (!response[storageKey]) {
+      console.log(storageKey + " doesn't exist in localStorage, attempting to populate now");
+      Service.get(Constants.definitionsUri, null, definitionsCallback);
+    } else {
+      console.log(storageKey + " value is stored already, as:", response);
+      Globals.artistLookupDefinitions = response["artistLookupDefinitions"];
+      console.log("set artistlookupdefs to :" + Globals.artistLookupDefinitions);
+    }
+  });
+}
 
 function definitionsCallback(response, callback) {
   console.log("Response is:" + response);
