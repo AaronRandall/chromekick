d8c5c573b4ce52e2229b07d0f309e95e84c5a6da
Aaron Randall <aaron@songkick.com>
2012-11-22 15:41:37 +0000

    Shy: Added definitions for thisismyjam and hypetrak.

--- scripts/songkick/definitions.js
+++ scripts/songkick/definitions.js
@@ -13,4 +13,6 @@ var localArtistLookupDefinitions = [
   {"Hostname": "thefader.com",             "Query": "//span[@class='tag-links-wide']//a[1]/text()"},
   {"Hostname": "spin.com",                 "Query": "//article[contains(@class,'article')]//footer/nav[@class='row']/div[contains(@class,'tagged')]/div[contains(@class,'field')]/div[@class='field-items']/a[1]/text()"},
   {"Hostname": "open.spotify.com",         "Query": "//h2/a/text()"}
+  {"Hostname": "thisismyjam.com",          "Query": "//div[@class='container']//h1[@id='jamArtist']/text()"},
+  {"Hostname": "hypetrak.com",             "Query": "//div[@class='artist']/div[@class='content']/text()"}
 ];
