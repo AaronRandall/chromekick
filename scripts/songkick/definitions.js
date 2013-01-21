8508b46428bf2ec7db50fe56c8f4d7bdc3dad7e5
Jamie Hughes <jamie@songkick.com>
2013-01-21 15:16:10 +0000

    fixed indentation for defini. file

--- scripts/songkick/definitions.js
+++ scripts/songkick/definitions.js
@@ -27,7 +27,7 @@ var localArtistLookupDefinitions = [
   {"Hostname": "ticketmaster.co.uk",       "Query": "//ol[@itemprop='breadcrumb']//li[contains(@id,'seo_bc_prodtitle')]/span"},
   {"Hostname": "twitter.com",              "Query": "//h1[contains(@class,'fullname')]/text()[1]"},
   {"Hostname": "bandcamp.com",             "Query": "//div[@id='name-section']//span[@itemprop='byArtist']/a/text()"},
-  {"Hostname": "musicfansmic.net", "Query": "//p/strong[1]/a/text()[1]"},
+  {"Hostname": "musicfansmic.net",         "Query": "//p/strong[1]/a/text()[1]"},
   {"Hostname": "last.fm",                  "Query": "//article[contains(@class,'artist-overview')]//h1[@itemprop='name']/text()"},
   {"Hostname": "hypem.com",                "Query": "//div/a[1][contains (@href,'/artist/')]/text()[1]"},
   {"Hostname": "rateyourmusic.com",        "Query": "//a[contains(@href, 'artist')]/text()"},
